fe7a9e8e3f1a096fc8fb38c0743ddbb4d71ba05f
IsratMoon <mnisrat.j@gmail.com>
2023-03-27 16:15:34 +0600

    Navbar Added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import './App.css';
+
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Header></Header>
+    </div>
+  );
+};
+
+export default App;
